2798c34499a14cde2e8cb3c2c63420ff0331f93f
jamin-xie-ics4u1-02-25 <Jamin.xie@student.tdsb.on.ca>
2025-04-04 02:36:04 +0000

    Update Change

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -22,10 +22,44 @@ public class PracticeProblem {
 		int mid;
 
 		while (low <= high) {
+			binaryLoops++;
 			mid = (low + high) / 2;
 			if (array[mid] == number) {
 				break;
 			} else if (array[mid] < number) {
+				low = mid + 1;
+			} else {
+				high = mid - 1;
+			}
+		}
+
+		return new int[]{sequentialLoops, binaryLoops};
+	}
+
+	public static int[] compareStringSearch(String[] array, String word) {
+		int sequentialLoops = 0;
+
+		for (int i = 0; i < array.length; i++) {
+			sequentialLoops++;
+			if (array[i].equals(word)) {
+				break;
+			}
+		}
+
+		Arrays.sort(array);
+		int binaryLoops = 0;
+		int low = 0;
+		int high = array.length - 1;
+		int mid;
+
+		while (low <= high) {
+			binaryLoops++;
+			mid = low + (high - low) / 2;
+			int comparison = array[mid].compareTo(word);
+
+			if (comparison == 0) {
+				break;
+			} else if (comparison < 0) {
 				low = mid + 1;
 			} else {
 				high = mid - 1;
